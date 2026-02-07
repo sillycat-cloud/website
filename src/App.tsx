3e2e9b7dc8214ff05772d09547b9f8b02bba14b9
6em-dev <rimit58872@gmail.com>
2026-02-07 12:42:41 +0100

    V1.5

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,27 @@ import ThemeIconToggle from "./components/ThemeIconToggle"
 import lightIcon from "./assets/light_mode_icon.png"
 import darkIcon from "./assets/dark_mode_icon.png"
 import { useTheme } from "./hooks/useTheme"
+import H1Text from "./components/h1_text"
 
 function App() {
   const { isDark } = useTheme()
+  
   return (
-    <div className={`${isDark ? 'bg-gray-900' : 'bg-yellow-50'} text-center min-h-screen`}>
-      <img src={isDark ? darkIcon : lightIcon} className="size-25 rounded-4xl shadow-black-5 inline-block" />
+    <div className={`${isDark ? 'bg-gray-900 text-yellow-50' : 'bg-yellow-50 text-black'} min-h-screen text-center gap-y-6 pt-3`}>
+      <img 
+        src={isDark ? darkIcon : lightIcon} 
+        className="w-24 h-24 rounded-3xl shadow-lg inline-block" 
+      />
       <ThemeIconToggle />
+    <div className="flex flex-col gap-y-5 w-screen bottom-5 items-center pt-15">
+      <H1Text>Welcome to the WIP Silly Cat Cloud homepage!</H1Text>
+      <button data-tally-open="pbBx1b" className="bg-blue-500 px-6 py-3 rounded text-white">
+        Join Waitlist
+      </button>
+      <button className="bg-green-500 py-4 rounded w-33 text-white">
+        Learn More
+      </button>
+      </div>
     </div>
   )
 }
---
+++ src/components/h1_text.tsx
@@ -0,0 +1,7 @@
+import type { ReactNode } from 'react';
+
+export default function H1Text({ children }: { children: ReactNode }) {
+    return (
+        <h1 className="font-bold text-4xl pt-5">{children}</h1>
+    );
+}
